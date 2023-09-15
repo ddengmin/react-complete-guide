902bb473a912d585f5633dcecd83d2fa1e67c804
ddengmin <ammin1127@naver.com>
2023-09-15 17:31:56 +0900

    ♻️ : props 날짜 코드 간결화시키기

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,13 +1,17 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const month = props.date.toLocaleString('en-US', { month : 'long'})
+  const day = props.date.toLocaleString('en-US', { day : '2-digit'})
+  const year = props.date.getFullYear()
+  //로직 추출 : JSX 함수에 넣지말고, 별도의 변수 만들기 - month(코드 간결화 목적) 
 
   return (
     <div className="expense-item">
       <div>
-        <div>{props.date.toLocaleString('en-US', { month : 'long'})}</div>
-        <div>Year</div>
-        <div>Day</div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
       </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>

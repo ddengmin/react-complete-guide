0ee9e078cbfeba38c25eee84b9cadd51499b20ce
ddengmin <ammin1127@naver.com>
2023-10-26 16:55:23 +0900

    ♻️ : react에 의해 평가된 ExpenseItem

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -5,7 +5,7 @@ import './ExpenseItem.css';
 
 export default function ExpenseItem(props) {
   const [title, setTitle] = useState(props.title);
-	
+	console.log('ExpenseItem evaluated by react component')
   const clickHandler = () => {
     setTitle('Updated!!');
     console.log(title);

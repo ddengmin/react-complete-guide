d8b4497479c1a674fca77ff2e55b4add8bb88274
ddengmin <ammin1127@naver.com>
2023-10-26 15:58:59 +0900

    ♻️ : state를 이용해 react에 적용시키기

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,14 @@
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 export default function ExpenseItem(props) {
-  let title = props.title
-
-	const clickHandler = () => {
-  title = 'Updated!';
-	console.log(title);
+  const [title, setTitle] = useState(props.title);
+	
+  const clickHandler = () => {
+    setTitle('Updated!!');
+    console.log(title);
 }
   return (
     <Card className='expense-item'>
